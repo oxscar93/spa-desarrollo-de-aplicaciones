62fefb89d71c875236badb41cd1d81ea506b0ea6
SGuille <guillesalvatore@gmail.com>
2021-10-28 10:24:50 -0300

    cambio api por cotización dolar + cambios

---
+++ backend/src/main/java/ar/edu/unq/desapp/grupoH022021/backenddesappapi/dto/DollarDto.java
@@ -0,0 +1,43 @@
+package ar.edu.unq.desapp.grupoH022021.backenddesappapi.dto;
+
+public class DollarDto {
+	private String compra;
+	private String venta;
+	private String fecha;
+	private String variacion;
+	private String classVariacion;
+	
+	public String getCompra() {
+		return compra;
+	}
+	public void setCompra(String compra) {
+		this.compra = compra;
+	}
+	public String getVenta() {
+		return venta;
+	}
+	public void setVenta(String venta) {
+		this.venta = venta;
+	}
+	public String getFecha() {
+		return fecha;
+	}
+	public void setFecha(String fecha) {
+		this.fecha = fecha;
+	}
+	public String getVariacion() {
+		return variacion;
+	}
+	public void setVariacion(String variacion) {
+		this.variacion = variacion;
+	}
+	public String getClassVariacion() {
+		return classVariacion;
+	}
+	public void setClassVariacion(String classVariacion) {
+		this.classVariacion = classVariacion;
+	}
+	
+	
+
+}
